a02da0d813ec513e0a40a796a57fe1eae2698507
Minjxnx <it20763312@my.sliit.lk>
2022-08-21 01:48:42 +0530

    Modified some pages

--- src/Admin components/Adminloginform.js
+++ src/Admin components/Adminloginform.js
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState} from 'react'
 import './Adminloginform.css'
 import { useNavigate } from "react-router-dom";
 import { db } from '../Firebase-config';
-import {collection,getDocs} from 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';
 
 function Adminloginform({ setAdminAuth }) {
 
     const [registerEmail, setRegisterEmail] = useState("");
     const [registerPassword, setRegisterPassword] = useState("");
 
-    const [adminUsers, setAdminUsers] =useState([]);
+    const [adminUsers, setAdminUsers] = useState([]);
     const adminUsersCollectionRef = collection(db, "adminUsers");
 
     const navigate = useNavigate();
 
-    useEffect (()=>{
-        const getUsers = async () =>{
+    useEffect(() => {
+        const getUsers = async () => {
             const data = await getDocs(adminUsersCollectionRef);
-            setAdminUsers(data.docs.map((doc)=>({...doc.data(),id: doc.id})));
+            setAdminUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
 
         }
         getUsers();
@@ -25,48 +25,49 @@ function Adminloginform({ setAdminAuth }) {
 
     const register = () => {
 
-        adminUsers.map((user)=>{
-            if(registerEmail===user.email && registerPassword===user.password){
+        adminUsers.map((user) => {
+            if (registerEmail === user.email && registerPassword === user.password) {
                 localStorage.setItem("adminAuth", true);
                 setAdminAuth(true);
                 alert("Signed in successfully!");
                 navigate('/adminhome');
             }
-            else{
+            else {
                 alert("Please enter the correct email or password");
             }
-            return(<></>);
+            return (<></>);
         })
 
     }
 
     return (
-        <div className="adminlogin-form-main">
-            <div className="adminlogin-form-sub-main">
-                <div className="adminlogin-form-sub-sub-main">
-                    <div className="adminlogin-form-imgs">
-                        <div className="adminlogin-form-container-image">
-                            <img src={process.env.PUBLIC_URL + "Logos/Login.png"} alt="profile" className="adminlogin-form-profile" />
+            <div className="adminlogin-form-main">
+                <div className="adminlogin-form-sub-main">
+                    <div className="adminlogin-form-sub-sub-main">
+                        <div className="adminlogin-form-imgs">
+                            <div className="adminlogin-form-container-image">
+                                <img src={process.env.PUBLIC_URL + "Logos/Login.png"} alt="profile" className="adminlogin-form-profile" />
+
+                            </div>
+
 
                         </div>
-
-
-                    </div>
-                    <div className='adminlogin-form-details'>
-                        <h1 className='adminlogin-form-heading'>Admin-Login</h1>
                         <div className='adminlogin-form-details'>
-                            <input type="text" placeholder="Email" className="adminlogin-form-name" onChange={(event) => { setRegisterEmail(event.target.value); }} />
-                        </div>
-                        <div className="adminlogin-form-second-input">
-                            <input type="password" placeholder="Password" className="adminlogin-form-name" onChange={(event) => { setRegisterPassword(event.target.value); }} />
-                        </div>
-                        <div className="adminlogin-form-login-button">
-                            <button onClick={register} className='adminlogin-form-button'>Login</button>
+                            <h1 className='adminlogin-form-heading'>Admin-Login</h1>
+                            <div className='adminlogin-form-details'>
+                                <input type="text" placeholder="Email" className="adminlogin-form-name" onChange={(event) => { setRegisterEmail(event.target.value); }} />
+                            </div>
+                            <div className="adminlogin-form-second-input">
+                                <input type="password" placeholder="Password" className="adminlogin-form-name" onChange={(event) => { setRegisterPassword(event.target.value); }} />
+                            </div>
+                            <div className="adminlogin-form-login-button">
+                                <button onClick={register} className='adminlogin-form-button'>Login</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+            
     )
 }
 
